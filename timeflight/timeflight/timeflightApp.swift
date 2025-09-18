a14ee44e628018793e66e93bc7cc40221b54a116
finn <bishoe01@gmail.com>
2025-09-18 21:38:55 +0900

    init: project initiation

---
+++ timeflight/timeflight/timeflightApp.swift
@@ -0,0 +1,17 @@
+//
+//  timeflightApp.swift
+//  timeflight
+//
+//  Created by bishoe01 on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct timeflightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
