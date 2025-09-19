ae021eec4af49a3ec75dbe11000db6af110b324d
finn <bishoe01@gmail.com>
2025-09-19 20:20:16 +0900

    chore: 디렉토리 구조 개선

---
+++ timeflight/timeflight/Common/StyleGuide.swift
@@ -0,0 +1,14 @@
+//
+//  StyleGuide.swift
+//  timeflight
+//
+//  Created by bishoe01 on 9/19/25.
+//
+
+import CoreGraphics
+
+enum Padding {
+    /// 타이머설정시 시계다이얼 좌우여백
+    static let outerCircleHorizontal: CGFloat = 38.0
+    static let innerCircleHorizontal: CGFloat = 49.0
+}
