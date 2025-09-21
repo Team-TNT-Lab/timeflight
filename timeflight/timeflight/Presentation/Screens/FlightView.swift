7b102031c4e5ae7d140d0090c79cdd60684255da
finn <bishoe01@gmail.com>
2025-09-21 16:35:40 +0900

    ui: 비행 현황 임시 ui 구현

--- timeflight/timeflight/Presentation/Screens/FlightView.swift
+++ timeflight/timeflight/Presentation/Screens/FlightView.swift
@@ -9,6 +9,27 @@ import SwiftUI
 
 struct FlightView: View {
     var body: some View {
-        Text("FlightView")
+        // 날짜섹션
+
+        // 현재 비행 현황
+        VStack(spacing: 10) {
+            Image(systemName: "airplane")
+                .font(.system(size: 24))
+            Text("비행까지 ?시간 남았어요")
+                .font(.system(size: 24))
+            Text("00:00에 수면시작").font(.system(size: 18))
+                .opacity(0.4)
+            Spacer().frame(height: 18)
+            Button("비행 시작") {
+                print("비행시작")
+            }.buttonStyle(.borderedProminent)
+                .tint(Color.buttongray)
+                .foregroundStyle(Color.black)
+                .controlSize(.large)
+        }
     }
+}
+
+#Preview {
+    FlightView()
 }
