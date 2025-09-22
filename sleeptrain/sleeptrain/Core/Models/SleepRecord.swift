bd635a878d0088c0b2b4da7f5d79f77e48db12cf
Yang Sijun (Air) <yang@sijun.dev>
2025-09-23 00:49:31 +0900

    [feat] 기본 모델(UserSettings, SleepRecord, Stats) 정의 (#11)
    * remove: 필요없는 파일 제거
    
    * feat: 기본 모델 정의
    
    * fix: Fix error cannot get/set with status in SleepRecord
    
    * feat: Add JourneyStatus String Value

---
+++ sleeptrain/sleeptrain/Core/Models/SleepRecord.swift
@@ -0,0 +1,44 @@
+//
+//  SleepRecord.swift
+//  sleeptrain
+//
+//  Created by 양시준 on 9/23/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class SleepRecord: Identifiable {
+    var id: UUID
+    var actualDepartureTime: Date
+    var actualArrivalTime: Date?
+    var status: JourneyStatus
+    var targetDepartureTime: Date
+    var targetArrivalTime: Date
+    
+    init(
+        id: UUID = UUID(),
+        actualDepartureTime: Date,
+        actualArrivalTime: Date? = nil,
+        status: JourneyStatus = .waitingToBoard,
+        targetDepartureTime: Date,
+        targetArrivalTime: Date
+    ) {
+        self.id = id
+        self.actualDepartureTime = actualDepartureTime
+        self.actualArrivalTime = actualArrivalTime
+        self.status = status
+        self.targetDepartureTime = targetDepartureTime
+        self.targetArrivalTime = targetArrivalTime
+    }
+}
+
+enum JourneyStatus: String, Codable {
+    case waitingToBoard = "waitingToBoard"
+    case onTrack = "onTrack"
+    case arrived = "arrived"
+    case delayed = "delayed"
+    case emergencyStop = "emergencyStop"
+    case cancelled = "cancelled"
+}
