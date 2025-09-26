a5e35ad67c75c8631d8a24e0c57399860a1fdfd0
gojaehyeon <gojaehyeonstudio@gmail.com>
2025-09-26 11:48:50 +0900

    feat: 카드 등록 / 삭제 부분 구현

---
+++ sleeptrain/sleeptrain/Features/Settings/View/CardManagementView.swift
@@ -0,0 +1,18 @@
+//
+//  CardManagementView.swift
+//  sleeptrain
+//
+//  Created by go on 9/26/25.
+//
+
+import SwiftUI
+
+struct CardManagementView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CardManagementView()
+}
